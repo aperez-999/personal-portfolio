2129f1c717e3503cd732c61286a9bd3ee2006e0f
Alejandro Perez- Rivero <alexperezr456@gmail.com>
2024-12-22 18:45:21 -0500

    Update AboutMe.jsx

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div>
             <img
-              src="/assets/images/about-me.jpg"
+              src="/personal-portfolio/assets/images/about-me.jpg"
               alt="About Me"
               width={400}
               height={400}
